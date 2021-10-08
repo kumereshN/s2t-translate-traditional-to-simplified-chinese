f83d11fdca0413a22da358fe6e40027907cd16ac
kumereshN <kumeresh6@hotmail.com>
2021-10-08 15:56:31 +0800

    added validation

--- main.py
+++ main.py
@@ -3,13 +3,18 @@
 from typing import Optional
 from fastapi import FastAPI, Query
 import chinese_converter as cc
+import re
 
 app = FastAPI()
 
 @app.get("/query/")
-async def translate_traditional_to_simplified(query_id: Optional[int] = None, query: Optional[str] = Query(None, alias="q")):
-    # convert traditional chinese to simplified chinese
-    chinese_simplified = cc.to_simplified(query)
-    results = {"traditional chinese": query, "simplified chinese": chinese_simplified}
-    return results
+async def translate_traditional_to_simplified(query_id: Optional[int] = 0, query: Optional[str] = Query(None, alias="q")):
+    # If there are Chinese characters present
+    if re.search("[\u4e00-\u9FFF]", query):
+        # convert traditional chinese to simplified chinese
+        chinese_simplified = cc.to_simplified(query)
+        results = {"query_id": query_id,"traditional chinese": query, "simplified chinese": chinese_simplified}
+        return results
+    else:
+        return 'Invalid query. Only accepts Chinese characters'
     
