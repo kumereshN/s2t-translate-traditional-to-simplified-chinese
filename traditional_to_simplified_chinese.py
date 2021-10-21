f007ab60a2458750e1bcbdb30e5bc8e6076dfe25
Kumeresh <kumeresh.n@s2t.ai>
2021-10-21 10:31:00 +0800

    updates

--- traditional_to_simplified_chinese.py
+++ traditional_to_simplified_chinese.py
@@ -2,6 +2,7 @@
 
 from typing import Optional
 from fastapi import FastAPI, Query
+import json
 import chinese_converter as cc
 import re
 
@@ -10,7 +11,7 @@ This API translates traditional Chinese to simplified Chinese.
 """
 app = FastAPI(title="Traditional to simplified Chinese translation API", description= description)
 
-@app.get("/query/", tags = ["Translate traditional Chinese to simplified Chinese"])
+@app.get("/translateQuery/", tags = ["Translate traditional Chinese to simplified Chinese"])
 async def translate_traditional_to_simplified_chinese(query_id: Optional[int] = 0, query: Optional[str] = Query(None, alias="q")):
     # If there are Chinese characters present
     if re.search("[\u4e00-\u9FFF]", query):
@@ -18,8 +19,36 @@ async def translate_traditional_to_simplified_chinese(query_id: Optional[int] = 
         chinese_simplified = cc.to_simplified(query)
         results = {"query_id": query_id,"traditional chinese": query, "simplified chinese": chinese_simplified}
         return results
+    elif query == "":
+        return 'You have not entered a query'
     else:
-        return 'Invalid query. Only accepts Chinese characters'
+        return 'Invalid query. Only Chinese characters are accepted.'
+
+@app.get("/translateQueryJson/", tags = ["JSON object: Translate traditional Chinese to simplified Chinese"])
+async def translate_traditional_to_simplified_chinese_json(query_id: Optional[int] = 0, query: Optional[str] = Query(None, alias="q")):
+    """
+    Check the type if it's a JSON string or a JSON object literal
+    """
+    # If it's a string, attempt to convert it to a JSON object literal
+    if type(query) == str:
+        query_json = json.loads(query)
+        # If after converting and it's not a dictionary, return "invalid query"
+        if not type(query_json) == dict:
+            return query_json
+    # If the query is not a dictionary, e.g: int, boolean
+    elif not type(query) == dict:
+        return "This is not a dictionary"
+    # If it's a valid query, set query_json as a variable
+    else:
+        query_json = query
+
+    # Transform the field's values into string format if they're not string
+    query_json_clean = {field:str(value) for field, value in query_json.items()}
+    # Convert Traditional chinese into Simplified Chinese
+    query_json_convert = {field:cc.to_simplified(value) for field, value in query_json_clean.items()}
+    results = {"query_id": query_id,"traditional chinese": query, "simplified chinese": query_json_convert}
+    return results
+
 
 if __name__ == "__main__":
     import uvicorn
