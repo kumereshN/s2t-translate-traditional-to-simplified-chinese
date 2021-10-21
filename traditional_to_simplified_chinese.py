915b8adb525f648300b4dec32a6c10d13a0d11cd
Kumeresh <kumeresh.n@s2t.ai>
2021-10-21 15:28:10 +0800

    backup

--- traditional_to_simplified_chinese.py
+++ traditional_to_simplified_chinese.py
@@ -12,12 +12,12 @@ This API translates traditional Chinese to simplified Chinese.
 app = FastAPI(title="Traditional to simplified Chinese translation API", description= description)
 
 @app.get("/translateQuery/", tags = ["Translate traditional Chinese to simplified Chinese"])
-async def translate_traditional_to_simplified_chinese(query_id: Optional[int] = 0, query: Optional[str] = Query(None, alias="q")):
+async def translate_traditional_to_simplified_chinese(queryID: Optional[int] = 0, query: Optional[str] = Query(None, alias="q")):
     # If there are Chinese characters present
     if re.search("[\u4e00-\u9FFF]", query):
         # convert traditional chinese to simplified chinese
         chinese_simplified = cc.to_simplified(query)
-        results = {"query_id": query_id,"traditional chinese": query, "simplified chinese": chinese_simplified}
+        results = {"queryID": queryID,"traditionalChinese": query, "simplifiedChinese": chinese_simplified}
         return results
     elif query == "":
         return 'You have not entered a query'
@@ -25,10 +25,11 @@ async def translate_traditional_to_simplified_chinese(query_id: Optional[int] = 
         return 'Invalid query. Only Chinese characters are accepted.'
 
 @app.get("/translateQueryJson/", tags = ["JSON object: Translate traditional Chinese to simplified Chinese"])
-async def translate_traditional_to_simplified_chinese_json(query_id: Optional[int] = 0, query: Optional[str] = Query(None, alias="q")):
+async def translate_traditional_to_simplified_chinese_json(queryId: Optional[int] = 0, query: Optional[str] = Query(None, alias="q")):
     """
-    Check the type if it's a JSON string or a JSON object literal
+    API to translate JSON object with traditional Chinese to simplified Chinese.
     """
+    return query
     # Use try and except to check if json.loads(query) works
     try:
         query_json = json.loads(query)
@@ -43,7 +44,7 @@ async def translate_traditional_to_simplified_chinese_json(query_id: Optional[in
     query_json_clean = {field:str(value) for field, value in query_json.items()}
     # Convert Traditional chinese into Simplified Chinese
     query_json_convert = {field:cc.to_simplified(value) for field, value in query_json_clean.items()}
-    results = {"query_id": query_id,"traditional chinese": query, "simplified chinese": query_json_convert}
+    results = {"queryId": queryId,"traditional_chinese": query, "simplified_chinese": query_json_convert}
     return results
 
 
